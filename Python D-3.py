259452458f89426401766b15aca83d0f4190423c
balqueues <145946403+balqueues@users.noreply.github.com>
2024-08-31 14:03:43 +0700

    Add files via upload

---
+++ Python D-3.py
@@ -0,0 +1,22 @@
+# Meminta pengguna memasukkan nilai awal
+nilai_awal = int(input("Masukkan nilai awal: "))
+
+# Menambahkan 2 ke nilai tersebut
+nilai_awal += 2
+print("Setelah ditambahkan 2, nilai menjadi:", nilai_awal)  # Output: 6
+
+# Mengurangi nilai tersebut dengan 3
+nilai_awal -= 3
+print("Setelah dikurangi 3, nilai menjadi:", nilai_awal)  # Output: 3
+
+# Mengalikan nilai tersebut dengan 10
+nilai_awal *= 10
+print("Setelah dikalikan 10, nilai menjadi:", nilai_awal)  # Output: 30
+
+# Membagi nilai tersebut dengan 4 dan mengonversinya kembali ke integer
+nilai_awal //= 4
+print("Setelah dibagi 4, nilai menjadi:", nilai_awal)  # Output: 7
+
+# Menghitung pangkat nilai tersebut dengan 10
+nilai_awal **= 10
+print("Setelah dipangkatkan 10, nilai menjadi:", nilai_awal)  # Output: 282475249
